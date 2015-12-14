dde2fcf191b878ac7ea59c2e20110469bdf20377
jaren <jlin@archos.com>
2015-12-14 15:01:56 +0800

    template pattern

---
+++ designpatterns/src/template/Demo.java
@@ -0,0 +1,10 @@
+package template;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("to easy, pls see the web!");
+	}
+
+}
